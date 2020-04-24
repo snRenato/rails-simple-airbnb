09fe7439063cddba505d7deddee2e7a238d63c85
Renato Souza <renatosneto0127@outlook.com>
2020-04-23 23:18:44 -0300

    Now you can share your favorite flats with your friends

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-$('.datepicker').pickadate();
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
